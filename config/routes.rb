ea0a5287e351d3369c1ffbe2e28156a66e239c81
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-23 04:30:46 +0800

    Setup Devise with default configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'page/home'
+  devise_for :users
+  root to: 'pages#home', as: :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/', to: 'pages#home', as: :home
 end
