d24b5592f197b7e243e4599cabe8207b523c9ca3
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-23 04:32:26 +0800

    Remove sanitizers for account edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :phone])
-
-    # For additional in app/views/devise/registrations/edit.html.erb
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
