d75e8fc3bb10375b3ef4a2e112699c1e1fbf81c0
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-23 06:25:50 +0800

    Clean up comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ApplicationRecord
   attr_accessor :login
 
-  enum role: [:volunteer, :admin]
+  enum role: [:admin, :volunteer]
   before_create :set_default_role
 
   validates :username, presence: :true, uniqueness: { case_sensitive: false }, length: { minimum: 6 }
-  # Only allow letter, number, underscore and punctuation. This is important so users don't create a username
-  # formatted like an email. Prevents issues where users might use an email as their username
+
+  # So users don't create a username formatted like an email. Prevents issues where users might use an email as their username
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
-  # Make the validation even more secure
-  validate :validate_username
+  validate :validate_username # Username must be unique
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -18,12 +17,8 @@ class User < ApplicationRecord
     @login || self.username || self.email
   end
 
-  # Because we want to change the behavior of the login action, we have to overwrite the find_for_database_authentication method.
-  # The method's stack works like this: find_for_database_authentication calls find_for_authentication which calls
-  # find_first_by_auth_conditions. Overriding the find_for_database_authentication method allows you to edit database
-  # authentication; overriding find_for_authentication allows you to redefine authentication at a specific point (such as token,
-  # LDAP or database). Finally, if you override the find_first_by_auth_conditions method, you can customize finder methods
-  # (such as authentication, account unlocking or password recovery).
+  # Overwrite find_for_database_authentication Devise default behavior for login action. Overriding this method allows us to edit
+  # the database authentication
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
