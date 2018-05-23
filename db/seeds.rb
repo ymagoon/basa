4e0ad867c54d80fbe89d8a517fbf7d4e8e21d8f4
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-23 17:47:09 +0800

    Seed volunteers, students and one admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # Seed subjects
+
 10.times do
   Subject.create(name: Faker::ProgrammingLanguage.name)
 end
@@ -12,15 +13,16 @@ end
 
 # Seed students and volunteers
 address = ""
+address2 = ""
 50.times do
   address = Address.new(venue_name: '',
-                         address_formatted: '',
-                         address_1: Faker::Address.street_address,
-                         address_2: Faker::Address.secondary_address,
-                         city: Faker::Address.city,
-                         state: Faker::Address.state,
-                         postal_code: Faker::Address.zip_code,
-                         address_type: rand(0..2)) # enumerator uses this
+                        address_formatted: '',
+                        address_1: Faker::Address.street_address,
+                        address_2: Faker::Address.secondary_address,
+                        city: Faker::Address.city,
+                        state: Faker::Address.state,
+                        postal_code: Faker::Address.zip_code,
+                        address_type: 2) # enumerator uses this
   address.save
 
   Student.create(first_name: Faker::Name.first_name,
@@ -31,40 +33,40 @@ address = ""
                  gender: rand(0..1),
                  address: address)
 
-  # address2 = Address.create(venue_name: '',
-  #                          address_formatted: '',
-  #                          address_1: Faker::Address.street_address,
-  #                          address_2: Faker::Address.secondary_address,
-  #                          city: Faker::Address.city,
-  #                          state: Faker::Address.state,
-  #                          postal_code: Faker::Address.zip_code,
-  #                          address_type: rand(0..2))
-
-  # User.create(first_name: Faker::Name.first_name,
-  #             last_name: Faker::Name.last_name,
-  #             email: Faker::Internet.email,
-  #             username: "#{Faker::Name.first_name}#{rand(0..3)}"
-  #             phone: Faker::PhoneNumber.phone_number,
-  #             address: address2,
-  #             role: 1)
+  # address2 = Address.new(venue_name: '',
+  #                        address_formatted: '',
+  #                        address_1: Faker::Address.street_address,
+  #                        address_2: Faker::Address.secondary_address,
+  #                        city: Faker::Address.city,
+  #                        state: Faker::Address.state,
+  #                        postal_code: Faker::Address.zip_code,
+  #                        address_type: 1)
+  # address2.save
+  User.create(first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              email: Faker::Internet.email,
+              username: "#{Faker::Name.first_name}#{rand(0..3)}",
+              password: '12345678',
+              phone: Faker::PhoneNumber.phone_number,
+              # address: address2,
+              role: 1)
 end
 
 # create admin
-# address2 = Address.create(venue_name: '',
-#                          address_formatted: '',
-#                          address_1: Faker::Address.street_address,
-#                          address_2: Faker::Address.secondary_address,
-#                          city: Faker::Address.city,
-#                          state: Faker::Address.state,
-#                          postal_code: Faker::Address.zip_code,
-#                          address_type: rand(0..2))
+address3 = Address.create(venue_name: '',
+                         address_formatted: '',
+                         address_1: Faker::Address.street_address,
+                         address_2: Faker::Address.secondary_address,
+                         city: Faker::Address.city,
+                         state: Faker::Address.state,
+                         postal_code: Faker::Address.zip_code,
+                         address_type: 1)
 
-# User.create(first_name: Faker::Name.first_name,
-#                last_name: Faker::Name.last_name,
-#                email: Faker::Internet.email,
-#                phone: Faker::PhoneNumber.phone_number,
-#                birth_date: Faker::Date.between(15.year.ago, 8.year.ago),
-#                gender: rand(0..1),
-#                address: address2,
-#                role: 0)
-
+User.create(first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            email: Faker::Internet.email,
+            username: "#{Faker::Name.first_name}#{rand(0..3)}",
+            password: '12345678',
+            phone: Faker::PhoneNumber.phone_number,
+            # address: address2,
+            role: 0)
