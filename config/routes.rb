d479044e97081390233ec1a79fa9186e31e64af0
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-23 14:04:44 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home', as: :home
+
+  resources :courses do
+    resources :sessions, except: [:new]
+    resources :attendances, only: [:index, :edit, :update]
+    resources :student_rosters, only: [:create, :destroy]
+    resources :volunteer_rosters, only: [:create, :destroy]
+  end
+
+  resources :subjects, only: :create
+  resources :students, only: [:index, :show, :create]
+  resources :volunteers, only: [:index, :show, :create]
+  resources :addresses, only: :create
 end
