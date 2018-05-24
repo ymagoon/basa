40cc19de63f23f634502b469b0f85e6d470f450a
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-24 14:19:20 +0800

    Move function to its own file

--- app/javascript/packs/dragdrop.js
+++ app/javascript/packs/dragdrop.js
@@ -1,70 +1,90 @@
 function dragAndDrop() {
   // preventing page from redirecting
-  $("html").on("dragover", function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      $("h1").text("Drag here");
+  const html = document.documentElement
+  const uploadArea = document.querySelector('.upload-area');
+
+  html.addEventListener('dragover',function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    // console.log(e);
   });
 
-  $("html").on("drop", function(e) { e.preventDefault(); e.stopPropagation(); });
-
-  // Drag enter
-  $('.upload-area').on('dragenter', function (e) {
-      e.stopPropagation();
-      e.preventDefault();
-      $("h1").text("Drop");
+  html.addEventListener('drop', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
   });
 
-  // Drag over
-  $('.upload-area').on('dragover', function (e) {
-      e.stopPropagation();
-      e.preventDefault();
-      $("h1").text("Drop");
+  uploadArea.addEventListener('dragenter', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
   });
 
-  // Drop
-  $('.upload-area').on('drop', function (e) {
-      e.stopPropagation();
-      e.preventDefault();
-
-      $("h1").text("Upload");
-
-      var file = e.originalEvent.dataTransfer.files;
-      var fd = new FormData();
-
-      fd.append('file', file[0]);
-
-      uploadData(fd);
+  uploadArea.addEventListener('dragover', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
   });
 
-  // Open file selector on div click
-  $("#uploadfile").click(function(){
-      $("#file").click();
-  });
+  uploadArea.addEventListener('drop', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
 
-  // file selected
-  $("#file").change(function(){
-      var fd = new FormData();
 
-      var files = $('#file')[0].files[0];
 
-      fd.append('file',files);
 
-      uploadData(fd);
+
+
+
+
+    console.log(e);
   });
 }
+
+export { dragAndDrop }
+
+//     // Drop
+//     $('.upload-area').on('drop', function (e) {
+//         e.stopPropagation();
+//         e.preventDefault();
+
+//         $("h1").text("Upload");
+
+//         var file = e.originalEvent.dataTransfer.files;
+//         var fd = new FormData();
+
+//         fd.append('file', file[0]);
+
+//         uploadData(fd);
+//     });
+
+//     // Open file selector on div click
+//     $("#uploadfile").click(function(){
+//         $("#file").click();
+//     });
+
+//     // file selected
+//     $("#file").change(function(){
+//         var fd = new FormData();
+
+//         var files = $('#file')[0].files[0];
+
+//         fd.append('file',files);
+
+//         uploadData(fd);
+//     });
+// });
+
 
     // Sending AJAX request and upload file
-function uploadData(formdata){
-  $.ajax({
-      url: 'upload.php',
-      type: 'post',
-      data: formdata,
-      contentType: false,
-      processData: false,
-      dataType: 'json',
-      success: function(response){
-          addThumbnail(response);
-      }
-  });
-}
+// function uploadData(formdata){
+//   $.ajax({
+//       url: 'upload.php',
+//       type: 'post',
+//       data: formdata,
+//       contentType: false,
+//       processData: false,
+//       dataType: 'json',
+//       success: function(response){
+//           addThumbnail(response);
+//       }
+//   });
+// }
