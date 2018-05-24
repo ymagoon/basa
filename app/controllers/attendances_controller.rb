4b902a5be053baef35be3e8884743fb6e015304b
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-25 00:28:03 +0800

    Fix query so the damn record updates

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,15 +2,13 @@ class AttendancesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:update]
 
   def update
-    @attendance = Attendance.where('student_id = ? AND session_id = ?', params[:student], params[:session])
-    # puts attendance.present?
+    # This will only ever return a single value
+    @attendance = Attendance.where('student_id = ? AND session_id = ?', params[:student], params[:session]).first
 
-    wtf
+    @attendance.present = @attendance.present? ? 'absent' : 'present'
+    @attendance.save
 
     render :json => { :wee => 'UNKNOWN_ERROR_TYPE'}, status: 200
   end
 
-  def wtf
-    @attendance.present? ? @attendance.update(present: 'absent') : @attendance.update(present: 'present')
-  end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,7 @@ class Attendance < ApplicationRecord
   belongs_to :student
   belongs_to :session
 
-  before_create :default_attendance
+  # before_create :default_attendance
 
   @presence = ['absent', 'present']
   enum present: @presence
@@ -10,6 +10,10 @@ class Attendance < ApplicationRecord
   def default_attendance
     self.present = 'absent'
   end
+
+  # def update
+  #   self.present? ? self.
+  # end
 
   validates :present, presence: true, inclusion: { in: @presence }
 end
