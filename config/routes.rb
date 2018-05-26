212247c5abf02d5b49ce11cf0f244369e94cc076
Cassy Myers <cassymyers@gmail.com>
2018-05-26 13:44:24 +0800

    change route, fix page

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :courses do
     resources :sessions, except: [:new, :create]
     resources :attendances, only: [:index]
-    resources :student_rosters, only: [:index, :destroy]
+    resources :student_rosters, only: [:index]
     patch '/attendance', to: 'attendances#update'
     post '/student_rosters/:student_id', to: 'student_rosters#create'
     resources :volunteer_rosters, only: [:new, :create]
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :show, :create]
   resources :addresses, only: [:create]
   resources :volunteer_rosters, only: [:destroy]
+  resources :student_rosters, only: [:destroy]
   resources :proficiencies, only: [:destroy]
 
   # note that the volunteer routes actually refer to users since there's no volunteer model. Where you see :volunteer_id => this refers to the :user_id. (e.g. in Proficiencies)
