56ad0ff24e35ec07ea1554e7ad69d2d9e0718c63
Cassy Myers <cassymyers@gmail.com>
2018-05-28 16:03:09 +0800

    more cleanup on aisle 3

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class DashboardController < ApplicationController
   before_action  :set_assigned_volunteers, :set_attendance_percentage, :set_active_courses
   def home
-    @courses = active_courses
-    @no_teacher = courses_with_no_teacher
+    @courses = @active_courses
+    @no_teacher = Course.no_teacher
     @below_cap = Course.courses_below_min_cap
   end
 
@@ -32,31 +32,6 @@ class DashboardController < ApplicationController
     Course.count
   end
 
-  def active_courses
-    @active_courses = Course.all.select { |c| c.status != 'cancelled'}
-    #NEED DATE FILTER
-  end
-
-  def active_course_attendance
-    total_attendance = @course.attendances.count
-    total_present = @course.attendances.select { |c| c.present == "present" }.count
-    @active_courses.each { |course|
-      present = course.attendances.select { |c| c.present == "present" }.count
-      total = course.attendances.count
-      return present/total
-    }
-    # list all courses that are active and their corresponding % aggregate
-  end
-
-  def courses_with_no_teacher
-    courses_w_teachers = VolunteerRoster.all.map { |a| a.course_id}.uniq
-    ids = Course.all.ids - courses_w_teachers
-    courses_with_no_teacher = ids.map { |id| Course.find(id) }
-  end
-
-
-
-
   # Students
   def total_number_of_students
     Student.count
@@ -68,36 +43,14 @@ class DashboardController < ApplicationController
     @attending_students = a.map { |a| Student.find(a.student_id) }.uniq
   end
 
-  def number_of_students_attended_course_unique
-    # distinct number of students attended course - count students that have attended > 1 course ONCE
-  end
-
   def total_student_attendance
     @percentage_attendance
-  end
-
-  def student_attendance_for_active_courses
-    # % of student attendance for active courses - make sure the % only counts attendance UNTIL TODAY! (day running report)
-    # otherwise, the data will be skewed
-
   end
 
   # Volunteers
   def total_number_of_volunteers
     User.where(role: 'volunteer').count
   end
-
-  # MOVED TO USER MODEL
-  # def number_of_volunteers_assigned_to_course
-  #    # total number of volunteers
-  #   users = User.where(role: 'volunteer').count
-
-  #   #total number of distinct users on volunteer_rosters
-  #   distinct_users = VolunteerRoster.select(:user_id).distinct.count
-
-  #    @assigned_volunteers = users - distinct_users
-  #   # show # of volunteers that have never been assigned to a course
-  # end
 
   def set_active_courses
     @active_courses = Course.all.select { |c| c.status != 'cancelled'}
@@ -118,4 +71,5 @@ class DashboardController < ApplicationController
 
     @assigned_volunteers = users - distinct_users
   end
+
 end
