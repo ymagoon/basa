bd95d0c98fe853d9fac9f01aec3945eb3aa7d2be
Cassy Myers <cassymyers@gmail.com>
2018-05-30 15:58:10 +0800

    add working tabsgit add .

--- app/javascript/packs/tabs.js
+++ app/javascript/packs/tabs.js
@@ -1,12 +1,19 @@
 
-function tabs() {
-  let tabs = document.getElementsByClassName("tab");
-  var i;
+// function tabs() {
+//   let tabs = document.getElementsByClassName("tab");
+//   var i;
 
-  for (i = 0; i < tabs.length; i++) {
-      tabs[i].addEventListener("click", function() {
-          this.classList.toggle("active");
-      });
-  }
+//   for (i = 0; i < tabs.length; i++) {
+//       tabs[i].addEventListener("click", function() {
+//           this.classList.toggle("active");
+//       });
+//   }
+// }
+
+function tabs() {
+  $("ul.nav-tabs a").click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
 }
 export { tabs };
