4424867d5a88df2e7056cc632132901ddd8092df
Cassy Myers <cassymyers@gmail.com>
2018-05-31 13:22:27 +0800

    add flexbox

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,15 @@
 class DashboardController < ApplicationController
-  before_action  :set_active_courses, :set_courses, :set_unassigned_vols, :set_subjects, :set_students, :set_attendances
+  before_action  :set_courses, :set_unassigned_vols, :set_subjects, :set_students, :set_attendances
 
   def home
+
+  @total_number_of_courses = Course.all
+  # @courses_last_week = @total_number_of_courses.count - @total_number_of_courses.last_week.count
+  @total_number_of_students = Student.all
+  # @students_last_week = @total_number_of_students.count - @total_number_of_students.last_week.count
+  @total_number_of_volunteers = User.volunteer.count
+    @students_group_by_age = @total_number_of_students.group_by_age
+    @students_group_by_gender = @total_number_of_students.group_by_gender
   end
 
   def statistics
@@ -11,33 +19,7 @@ class DashboardController < ApplicationController
     @users = User.all
   end
 
- def statistics
-  @total_number_of_courses = Course.all
-  # @courses_last_week = @total_number_of_courses.count - @total_number_of_courses.last_week.count
-  @total_number_of_students = Student.all
-  # @students_last_week = @total_number_of_students.count - @total_number_of_students.last_week.count
-  @total_number_of_volunteers = User.volunteer.count
-  # @volunteers_last_week = @total_number_of_volunteers.count - @total_number_of_volunteers.last_week.count
 
-
-
-
-  # @courses_last_week = @total_number_of_courses - Course.where('created_at < ?', DateTime.now - 7).count
-
-  #   @total_number_of_students = Student.count
-  #   @students_last_week = @total_number_of_students - Student.where('created_at < ?', DateTime.now - 7).count
-
-  #   @total_number_of_volunteers = User.where(role: 'volunteer').count
-  #   @volunteers_last_week = @total_number_of_volunteers - User.where('role = ? and created_at < ?', 1, DateTime.now - 7).count
-  # @teachers = User.all
-
-  # @proficiencies
-  # @subjects = Subject.all
-  # @users = User.all
-
-  @students_group_by_age = @total_number_of_students.group_by_age
-  @students_group_by_gender = @total_number_of_students.group_by_gender
- end
 
   private
 
--- app/javascript/packs/charts.js
+++ app/javascript/packs/charts.js
@@ -4,8 +4,10 @@ import Chart from "chart.js";
 Chartkick.addAdapter(Chart);
 
 Chartkick.options = {
-  height: "400px",
-  colors: ["#77C9D4", "#57BC90"]
+  height: "200px",
+  width: "200px",
+  colors: ["#77C9D4", "#57BC90", "#B9929F", "#E2C2C6", "#B47EB3", "#E85D75", "#077187", "#D9F0FF" ],
+  legend: "bottom"
 }
 
 window.Chartkick = Chartkick;
