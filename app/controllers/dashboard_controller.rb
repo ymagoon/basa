157d0142f7286a2a6587e7d08ca361807797d9d8
Yitzhak Magoon <ymagoon@gmail.com>
2018-05-31 12:16:49 +0800

    add group_by_age and group_by_gender

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,12 +14,31 @@ class DashboardController < ApplicationController
   end
 
  def statistics
-  @courses = Course.all
-  @teachers = User.all
-  @students = Student.all
-  @proficiencies
-  @subjects = Subject.all
-  @users = User.all
+  @total_number_of_courses = Course.all
+  # @courses_last_week = @total_number_of_courses.count - @total_number_of_courses.last_week.count
+  @total_number_of_students = Student.all
+  # @students_last_week = @total_number_of_students.count - @total_number_of_students.last_week.count
+  @total_number_of_volunteers = User.volunteer.count
+  # @volunteers_last_week = @total_number_of_volunteers.count - @total_number_of_volunteers.last_week.count
+
+
+
+
+  # @courses_last_week = @total_number_of_courses - Course.where('created_at < ?', DateTime.now - 7).count
+
+  #   @total_number_of_students = Student.count
+  #   @students_last_week = @total_number_of_students - Student.where('created_at < ?', DateTime.now - 7).count
+
+  #   @total_number_of_volunteers = User.where(role: 'volunteer').count
+  #   @volunteers_last_week = @total_number_of_volunteers - User.where('role = ? and created_at < ?', 1, DateTime.now - 7).count
+  # @teachers = User.all
+
+  # @proficiencies
+  # @subjects = Subject.all
+  # @users = User.all
+
+  @students_group_by_age = @total_number_of_students.group_by_age
+  @students_group_by_gender = @total_number_of_students.group_by_gender
  end
 
   private
--- app/models/student.rb
+++ app/models/student.rb
@@ -16,6 +16,10 @@ class Student < ApplicationRecord
   validates :birth_date, presence: true
   validates :gender, presence: true, inclusion: { in: @gender }
 
+  scope :group_by_gender, -> { group(:gender).count }
+  scope :group_by_age, -> { self.each_with_object(Hash.new(0)) { |obj, counts| counts[obj.age2] += 1 } }
+  # scope :last_week, -> { where('created_at < ?', DateTime.now - 7) }
+
   # Return a list of all student attendance for a specified course. Used in course show to properly display the attendance
   def student_attendance(course)
     Attendance.joins("INNER JOIN sessions ON sessions.id = attendances.session_id and attendances.student_id = #{self.id} INNER JOIN courses ON sessions.course_id = courses.id and courses.id= #{course.id} order by attendances.id")
@@ -24,6 +28,10 @@ class Student < ApplicationRecord
   def age(dob)
     now = Date.today
     now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
+
+  def age2
+    DateTime.now.year - self.birth_date.year
   end
 
   def birth_date_formatted
